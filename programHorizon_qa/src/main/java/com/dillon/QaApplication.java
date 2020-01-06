aebc263858b3873485ef78792aefdbbecd0af4e9
dillon Xie <dillon.xie@oocl.com>
2020-01-06 22:07:54 +0800

    fix some bug about jar

--- programHorizon_qa/src/main/java/com/dillon/QaApplication.java
+++ programHorizon_qa/src/main/java/com/dillon/QaApplication.java
@@ -3,7 +3,6 @@ package com.dillon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -17,7 +16,6 @@ import util.JwtUtil;
  */
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
 public class QaApplication {
--- programhorizon_spit/src/main/java/com/dillon/SpitApplication.java
+++ programhorizon_spit/src/main/java/com/dillon/SpitApplication.java
@@ -3,7 +3,7 @@ package com.dillon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
 
@@ -14,7 +14,7 @@ import util.IdWorker;
  */
 @SpringBootApplication
 @EnableCaching
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class SpitApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpitApplication.class, args);
